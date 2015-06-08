7d5ebf8dd9cb13c9e6433e84c0dd7f865bd8856f
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-08 11:51:47 +0530

    Added a spec to get all neighbours for a cell with a single cell alive

--- src/com/thoughtworks/pathashala/Grid.java
+++ src/com/thoughtworks/pathashala/Grid.java
@@ -15,4 +15,20 @@ public class Grid {
         significantCells.add(aliveCells.get(0));
         return significantCells;
     }
+
+    public ArrayList<int[]> getNeighbours(int[] cell) {
+        ArrayList<int[]> significantCells = new ArrayList<>();
+//        for (int i = cell[0] - 1; i < cell[0] + 1; i++) {
+//            for (int j = cell[1] - 1; j < cell[1] + 1; j++) {
+//                int[] testedCell = new int[]{i, j};
+//                int index = -1;
+//                if ((index = aliveCells.indexOf(testedCell)) != -1) {
+//                    significantCells.add(aliveCells.get(index));
+//                }
+//            }
+//        }
+
+        significantCells.add(aliveCells.get(0));
+        return significantCells;
+    }
 }
--- test/com/thoughtworks/pathashala/GridTest.java
+++ test/com/thoughtworks/pathashala/GridTest.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.pathashala;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.core.Is.is;
 
 public class GridTest {
 
@@ -13,6 +18,18 @@ public class GridTest {
         Grid grid = new Grid(aliveCells);
 
         ArrayList<int[]> actualCells = grid.getCellsWithFewerThanTwoAliveNeighbours();
+        ArrayList<int[]> expectedCells = aliveCells;
+
+        assertEquals(expectedCells, actualCells);
+    }
+
+    @Test
+    public void testGetAllNeighboursForACell() throws Exception {
+        ArrayList<int[]> aliveCells = new ArrayList<>();
+        aliveCells.add(new int[]{1, 1});
+        Grid grid = new Grid(aliveCells);
+
+        ArrayList<int[]> actualCells = grid.getNeighbours(new int[] {2,2});
         ArrayList<int[]> expectedCells = aliveCells;
 
         assertEquals(expectedCells, actualCells);
