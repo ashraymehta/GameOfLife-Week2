7068ea6cf5d3e0d0a4a4beef056b2828bc9387d3
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-08 11:57:07 +0530

    Added a spec in Cell to check that two cells with different positions are not equal

--- test/com/thoughtworks/pathashala/CellTest.java
+++ test/com/thoughtworks/pathashala/CellTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.pathashala;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class CellTest {
     @Test
@@ -11,5 +12,13 @@ public class CellTest {
         Cell replicaCell = new Cell(1, 1);
 
         assertEquals(cell, replicaCell);
+    }
+
+    @Test
+    public void testCellsAreNotEqualWhenTheyreDontHaveTheSamePosition() throws Exception {
+        Cell firstCell = new Cell(1, 1);
+        Cell secondCell = new Cell(3, 3);
+
+        assertNotEquals(firstCell, secondCell);
     }
 }
