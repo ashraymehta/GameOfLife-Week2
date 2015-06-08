ec1dae15f909b87b490cc7d0c4c66019716ba2ac
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-08 13:45:27 +0530

    Removed getNeighbours method from Grid class

--- src/com/thoughtworks/pathashala/Grid.java
+++ src/com/thoughtworks/pathashala/Grid.java
@@ -15,18 +15,4 @@ public class Grid {
         significantCells.add(aliveCells.get(0));
         return significantCells;
     }
-
-    public ArrayList<Cell> getNeighbours(Cell cell) {
-        ArrayList<Cell> significantCells = new ArrayList<>();
-//        for (int i = cell[0] - 1; i < cell[0] + 1; i++) {
-//            for (int j = cell[1] - 1; j < cell[1] + 1; j++) {
-//                int[] testedCell = new int[]{i, j};
-//                int index = -1;
-//                if ((index = aliveCells.indexOf(testedCell)) != -1) {
-//                    significantCells.add(aliveCells.get(index));
-//                }
-//            }
-//        }
-        return aliveCells;
-    }
 }
